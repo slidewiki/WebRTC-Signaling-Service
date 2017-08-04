ed950f33b8e6d22e7d41ae72ba903bcd49fd3406
Roy Meissner <meissner@informatik.uni-leipzig.de>
2017-08-04 12:47:07 +0200

    Corrected Dockerhub repo

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t slidewiki/microservicetemplate ./
-docker push slidewiki/microservicetemplate
+docker build -t slidewiki/webrtcstunservice ./
+docker push slidewiki/webrtcstunservice
